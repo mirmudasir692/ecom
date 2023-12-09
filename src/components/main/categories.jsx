47b312473abb8939f2a4a1d53905c78e1f606953
mirmudasir692 <118063314+mirmudasir692@users.noreply.github.com>
2023-12-09 14:59:11 +0530

    Update categories.jsx

--- src/components/main/categories.jsx
+++ src/components/main/categories.jsx
@@ -27,7 +27,7 @@ function Categories(){
   
            {categories && categories.map((category)=>(
              <div key={category.id}>
-             <p className="text-start text-3xl pb-1">Stationary</p>
+             <p className="text-start text-3xl pb-1">{category.name}</p>
              <Link to={`/products/${category.id}`}>
              <img className="h-96 w-full rounded-lg" src={`https://mudasir12345.pythonanywhere.com/media/${category.image}`} alt="" />
              </Link>
