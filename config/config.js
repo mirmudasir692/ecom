73c6a68322a3ce23b0dc00c1e799e3dfa1c83a40
mirmudasir692 <mirmudasir692@gmail.com>
2023-12-13 17:26:17 +0530

    update

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 const BaseEndUrl = import.meta.env.VITE_BACKEND_BASEURL;
-export const BaseFrontUrl = import.meta.VITE_FRONTEND_BASEURL
+export const BaseFrontUrl = import.meta.env.VITE_FRONTEND_BASEURL
 
 export default BaseEndUrl
